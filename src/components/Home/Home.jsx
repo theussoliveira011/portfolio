eb243f56c74dfbd3182ca82b8a3137ac1c186ee9
Matheus <matheusfotografia987@gmail.com>
2023-04-01 15:33:25 -0300

    feat:create-Home-Component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+export default function Home(){
+  return (
+    <main>
+      <div className="name-title">
+        <h1>Matheus</h1>
+        <p>Web Developer Full Stack</p>
+      </div>
+    </main>
+  );
+
+};
