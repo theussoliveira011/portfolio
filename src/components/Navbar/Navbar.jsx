4ec62fdcd6287a5efcbf561989a4d12958683946
Matheus <matheusfotografia987@gmail.com>
2023-04-01 15:32:40 -0300

    feat:create-navbar-component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+import { Link } from 'react-router-dom';
+
+export default function NavBar(){
+  return (
+    <section className="menu-header">
+      <div className="menu-box-list">
+        <ul className="menu-bar-list">
+          <Link className='list' to="/" exact="true">Home</Link>
+          <Link className='list' to="skills">Skills</Link>
+          <Link className='list' to="about">About</Link>
+          <Link className='list' to="contact">Contact</Link>
+        </ul>
+      </div>
+    </section>
+  );
+};
